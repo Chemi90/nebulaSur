b0274e02897e60888f2c81b684bf1706c7e2f72e
Jose Miguel <xemiruiz@gmail.com>
2026-02-17 12:09:27 +0100

    fix: url video chatboti

--- src/data/workVideos.js
+++ src/data/workVideos.js
@@ -15,7 +15,7 @@ const workVideos = [
   },
   {
     id: 'aiStoreChat',
-    embedUrl: 'https://youtube.com/shorts/p3657CGdngE?feature=share',
+    embedUrl: 'https://www.youtube.com/shorts/p3657CGdngE',
     titleKey: 'Projects.items.aiStoreChat.title',
     descriptionKey: 'Projects.items.aiStoreChat.description'
   }
