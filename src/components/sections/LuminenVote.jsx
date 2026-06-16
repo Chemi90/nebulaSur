b72ea1a46dbdcd377847ff63aa4385a66228a9ef
Jose Miguel <xemiruiz@gmail.com>
2026-06-16 23:23:02 +0200

    Open Luminen vote link in same tab

--- src/components/sections/LuminenVote.jsx
+++ src/components/sections/LuminenVote.jsx
@@ -19,8 +19,6 @@ export default function LuminenVote() {
           <a
             className="btn btn-lumen"
             href={VOTE_URL}
-            target="_blank"
-            rel="noopener noreferrer"
           >
             {t('luminen.cta')}
           </a>
